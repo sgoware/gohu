d52b19f1cabc4315e708f9f4bed9f9eae8ca1205
StellarisW <744637972@qq.com>
2022-08-24 16:13:15 +0800

    fix: package err

---
+++ app/service/oauth/api/internal/handler/refreshtokenhandler.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"main/app/common/log"
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"main/app/service/oauth/api/internal/logic"
+	"main/app/service/oauth/api/internal/svc"
+	"main/app/service/oauth/api/internal/types"
+)
+
+func RefreshTokenHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.GetTokenByRefreshTokenReq
+		logger := log.GetSugaredLogger()
+
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			logger.Errorf("Parse http args failed, \nrequest: \n%v, \nerr: \n%v", r, err)
+			return
+		}
+		logger.Debugf("recv args: %v", req)
+
+		l := logic.NewRefreshTokenLogic(r.Context(), svcCtx)
+
+		res, err := l.RefreshToken(&req)
+		if err != nil {
+			logger.Errorf("Process logic failed, err: %v", err)
+		}
+
+		logger.Info("response: %v", res)
+		httpx.WriteJson(w, res.Code, res)
+	}
+}
