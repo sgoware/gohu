0214e622fadc68ec4691f37be2c2483435ada168
StellarisW <744637972@qq.com>
2022-08-25 12:01:54 +0800

    refactor: style

--- app/common/config/config.go
+++ app/common/config/config.go
@@ -7,7 +7,6 @@ import (
 	"github.com/apolloconfig/agollo/v4/constant"
 	agolloConfig "github.com/apolloconfig/agollo/v4/env/config"
 	"github.com/apolloconfig/agollo/v4/extension"
-	"github.com/go-redis/redis/v8"
 	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/viper"
 	"main/app/common/log"
@@ -133,42 +132,6 @@ func (c *Agollo) UnmarshalKey(namespace, key string, dst interface{}) (err error
 		decoderConfig.Squash = true
 	})
 	return
-}
-
-// GetMysqlDsn 返回 mysql DSN
-func (c *Agollo) GetMysqlDsn(namespace string) string {
-	config := c.client.GetConfig(namespace)
-	serverNum := config.GetIntValue("database.mysql.serverNum", 0) // 使用 mysql 几号服务器(考虑以后将数据库分布式化,使用 TiDB )
-
-	databaseConfig := c.client.GetConfig(databaseNamespace)
-
-	// 拼接dsn字符串
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true&loc=Asia%%2FShanghai",
-		databaseConfig.GetValue(fmt.Sprintf("mysql.server%d.username", serverNum)), // 数据库用户名
-		databaseConfig.GetValue(fmt.Sprintf("mysql.server%d.password", serverNum)), // 数据库密码
-		databaseConfig.GetValue(fmt.Sprintf("mysql.server%d.address", serverNum)),  // 数据库地址
-		databaseConfig.GetValue(fmt.Sprintf("mysql.server%d.port", serverNum)),     // 数据库端口
-		config.GetValue("database.mysql.databaseName"),                             // mysql 的数据库名字
-		config.GetValue("database.mysql.databaseCharset"),                          // mysql 的数据库使用的字符集
-	)
-	return dsn
-}
-
-// NewRedisOptions 返回 *redisOptions
-func (c *Agollo) NewRedisOptions(namespace string) *redis.Options {
-	config := c.client.GetConfig(namespace)
-	serverNum := config.GetIntValue("database.redis.serverNum", 0) // 使用 redis 几号服务器
-
-	databaseConfig := c.client.GetConfig(databaseNamespace)
-
-	return &redis.Options{
-		Addr: fmt.Sprintf("%s:%s",
-			databaseConfig.GetValue(fmt.Sprintf("redis.server%d.address", serverNum)),
-			databaseConfig.GetValue(fmt.Sprintf("redis.server%d.port", serverNum)),
-		),
-		Password: databaseConfig.GetValue(fmt.Sprintf("redis.server%d.password", serverNum)),
-		DB:       config.GetIntValue("database.redis.databaseNum", 0), // 使用 redis 几号数据库
-	}
 }
 
 func (c *Agollo) GetClientDetails() (clientAuths map[string]interface{}) {
