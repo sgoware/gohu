e1714e453c1fd7deb9450e7e75fd06e102ff0124
StellarisW <744637972@qq.com>
2022-08-29 22:59:10 +0800

    fix: sql bug

--- app/service/comment/rpc/crud/internal/logic/publishcommentlogic.go
+++ app/service/comment/rpc/crud/internal/logic/publishcommentlogic.go
@@ -35,25 +35,80 @@ func (l *PublishCommentLogic) PublishComment(in *pb.PublishCommentReq) (res *pb.
 	commentIndexModel := l.svcCtx.CommentModel.CommentIndex
 	commentContentModel := l.svcCtx.CommentModel.CommentContent
 
-	err = commentIndexModel.WithContext(l.ctx).
-		Create(&model.CommentIndex{
-			SubjectID:    in.SubjectId,
-			UserID:       j.Get("user_id").Int(),
-			IPLoc:        ip.GetIpLocFromApi(j.Get("last_ip").String()),
-			RootID:       in.RootId,
-			CommentFloor: in.CommentFloor,
-			CommentID:    in.CommentId,
-			ReplyFloor:   in.ReplyFloor,
-		})
-	if err != nil {
-		logger.Errorf("publish comment failed, err: mysql err, %v", err)
-		res = &pb.PublishCommentRes{
-			Code: http.StatusInternalServerError,
-			Msg:  "internal err",
-			Ok:   false,
+	if in.RootId == 0 {
+		// 是评论的情况
+		count, err := commentIndexModel.WithContext(l.ctx).
+			Where(commentIndexModel.SubjectID.Eq(in.SubjectId),
+				commentIndexModel.RootID.Eq(0)).
+			Count()
+		if err != nil {
+			logger.Errorf("publish comment failed, err: mysql err, %v", err)
+			res = &pb.PublishCommentRes{
+				Code: http.StatusInternalServerError,
+				Msg:  "internal err",
+				Ok:   false,
+			}
+			logger.Debugf("send message: %v", res.String())
+			return res, nil
 		}
-		logger.Debugf("send message: %v", res.String())
-		return res, nil
+
+		err = commentIndexModel.WithContext(l.ctx).
+			Create(&model.CommentIndex{
+				SubjectID:    in.SubjectId,
+				UserID:       j.Get("user_id").Int(),
+				IPLoc:        ip.GetIpLocFromApi(j.Get("last_ip").String()),
+				RootID:       0,
+				CommentFloor: int32(count + 1),
+				CommentID:    0,
+				ReplyFloor:   0,
+			})
+		if err != nil {
+			logger.Errorf("publish comment failed, err: mysql err, %v", err)
+			res = &pb.PublishCommentRes{
+				Code: http.StatusInternalServerError,
+				Msg:  "internal err",
+				Ok:   false,
+			}
+			logger.Debugf("send message: %v", res.String())
+			return res, nil
+		}
+	} else {
+		// 是回复评论的情况
+		count, err := commentIndexModel.WithContext(l.ctx).
+			Where(commentIndexModel.SubjectID.Eq(in.SubjectId),
+				commentIndexModel.CommentID.Eq(in.CommentId)).
+			Count()
+		if err != nil {
+			logger.Errorf("publish comment failed, err: mysql err, %v", err)
+			res = &pb.PublishCommentRes{
+				Code: http.StatusInternalServerError,
+				Msg:  "internal err",
+				Ok:   false,
+			}
+			logger.Debugf("send message: %v", res.String())
+			return res, nil
+		}
+
+		err = commentIndexModel.WithContext(l.ctx).
+			Create(&model.CommentIndex{
+				SubjectID:    in.SubjectId,
+				UserID:       j.Get("user_id").Int(),
+				IPLoc:        ip.GetIpLocFromApi(j.Get("last_ip").String()),
+				RootID:       in.RootId,
+				CommentFloor: 0,
+				CommentID:    in.CommentId,
+				ReplyFloor:   int32(count + 1),
+			})
+		if err != nil {
+			logger.Errorf("publish comment failed, err: mysql err, %v", err)
+			res = &pb.PublishCommentRes{
+				Code: http.StatusInternalServerError,
+				Msg:  "internal err",
+				Ok:   false,
+			}
+			logger.Debugf("send message: %v", res.String())
+			return res, nil
+		}
 	}
 
 	commentIndex, err := commentIndexModel.WithContext(l.ctx).
