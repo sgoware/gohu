0ad66fa07843f5fd87374722a440b28e65a5a9e9
StellarisW <744637972@qq.com>
2022-09-02 17:13:51 +0800

    fix: structx

--- app/utils/structx/struct.go
+++ app/utils/structx/struct.go
@@ -1,8 +1,9 @@
 package structx
 
 import (
-	"fmt"
 	"reflect"
+	"strings"
+	"time"
 )
 
 func SyncWithNoZero(src interface{}, dst interface{}) (err error) {
@@ -11,20 +12,48 @@ func SyncWithNoZero(src interface{}, dst interface{}) (err error) {
 
 	tDst := reflect.TypeOf(dst).Elem()
 	vDst := reflect.ValueOf(dst).Elem()
-	for i := 0; i < tSrc.NumField(); i++ {
-		if vSrc.Field(i).IsZero() {
+	srcNumField := tSrc.NumField()
+	dstNumField := tDst.NumField()
+	vis := make([]bool, 100)
+	for i := 0; i < srcNumField; i++ {
+		if vSrc.Field(i).IsZero() || !tSrc.Field(i).IsExported() {
 			continue
 		}
-		if !tSrc.Field(i).IsExported() || !tDst.Field(i).IsExported() {
-			continue
+		for j := 0; j < dstNumField; j++ {
+			if vis[j] {
+				continue
+			}
+			if !tDst.Field(j).IsExported() {
+				continue
+			}
+			if strings.ToLower(tSrc.Field(i).Name) != strings.ToLower(tDst.Field(j).Name) {
+				continue
+			}
+			if tSrc.Field(i).Type != tDst.Field(j).Type {
+				if tSrc.Field(i).Type == reflect.TypeOf(time.Time{}) {
+					if !vDst.Field(j).CanSet() {
+						continue
+					}
+					vDst.Field(j).Set(reflect.ValueOf(vSrc.Field(i).Interface().(time.Time).String()))
+					vis[j] = true
+					break
+				} else if tDst.Field(j).Type == reflect.TypeOf(time.Time{}) {
+					if !vDst.Field(j).CanSet() {
+						continue
+					}
+					t, _ := time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", vSrc.Field(i).Interface().(string))
+					vDst.Field(j).Set(reflect.ValueOf(t))
+					vis[j] = true
+					break
+				}
+			}
+			if !vDst.Field(j).CanSet() {
+				continue
+			}
+			vDst.Field(j).Set(vSrc.Field(i))
+			vis[j] = true
+			break
 		}
-		if tSrc.Field(i).Type != tDst.Field(i).Type {
-			return fmt.Errorf("field: %s is not the same", tSrc.Field(i).Name)
-		}
-		if !vDst.Field(i).CanSet() {
-			return fmt.Errorf("field: %s cannot set", tSrc.Field(i).Name)
-		}
-		vDst.Field(i).Set(vSrc.Field(i))
 	}
 	return nil
 }
