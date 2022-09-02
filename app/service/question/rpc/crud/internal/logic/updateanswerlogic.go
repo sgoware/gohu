3ca4e90bdb8c0b6dfd735cefc1dbb059c92a16b1
StellarisW <744637972@qq.com>
2022-09-03 07:45:19 +0800

    feat: add cache support for update question

--- app/service/question/rpc/crud/internal/logic/updateanswerlogic.go
+++ app/service/question/rpc/crud/internal/logic/updateanswerlogic.go
@@ -2,9 +2,14 @@ package logic
 
 import (
 	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"google.golang.org/protobuf/proto"
 	"gorm.io/gorm"
 	"main/app/common/log"
+	modelpb "main/app/service/question/dao/pb"
 	"net/http"
+	"time"
 
 	"main/app/service/question/rpc/crud/internal/svc"
 	"main/app/service/question/rpc/crud/pb"
@@ -30,33 +35,108 @@ func (l *UpdateAnswerLogic) UpdateAnswer(in *pb.UpdateAnswerReq) (res *pb.Update
 	logger := log.GetSugaredLogger()
 	logger.Debugf("recv message: %v", in.String())
 
+	nowTime := time.Now()
+
 	answerContentModel := l.svcCtx.QuestionModel.AnswerContent
 
-	_, err = answerContentModel.WithContext(l.ctx).Select(answerContentModel.Content).
-		Where(answerContentModel.AnswerID.Eq(in.AnswerId)).
-		Update(answerContentModel.Content, in.Content)
-	switch err {
-	case gorm.ErrRecordNotFound:
-		res = &pb.UpdateAnswerRes{
-			Code: http.StatusBadRequest,
-			Msg:  "answer not found",
-			Ok:   false,
+	answerContentBytes, err := l.svcCtx.Rdb.Get(l.ctx,
+		fmt.Sprintf("answer_content", in.AnswerId)).Bytes()
+	if err == nil {
+		answerContentProto := &modelpb.AnswerContent{}
+		err = proto.Unmarshal(answerContentBytes, answerContentProto)
+		if err != nil {
+			logger.Errorf("unmarshal [answerContentProto] failed, err: %v", err)
+		} else {
+			answerContentProto.AnswerId = in.AnswerId
+			answerContentProto.Content = in.Content
+			answerContentProto.UpdateTime = nowTime.String()
+
+			answerContentBytes, err = proto.Marshal(answerContentProto)
+			if err != nil {
+				logger.Errorf("marshal [answerContentProto] failed, err: %v", err)
+				res = &pb.UpdateAnswerRes{
+					Code: http.StatusInternalServerError,
+					Msg:  "internal err",
+					Ok:   false,
+				}
+				logger.Debugf("send message: %v", err)
+				return res, nil
+			}
+			err = l.svcCtx.Rdb.Set(l.ctx,
+				fmt.Sprintf("answer_content_%d", in.AnswerId),
+				answerContentBytes,
+				time.Second*86400).Err()
+			if err != nil {
+				logger.Errorf("set [answer_content] cache failed, err: %v", err)
+				res = &pb.UpdateAnswerRes{
+					Code: http.StatusInternalServerError,
+					Msg:  "internal err",
+					Ok:   false,
+				}
+				logger.Debugf("send message: %d", err)
+				return res, nil
+			}
 		}
-	case nil:
-		res = &pb.UpdateAnswerRes{
-			Code: http.StatusOK,
-			Msg:  "update answer successfully",
-			Ok:   true,
+	} else {
+		if err != redis.Nil {
+			logger.Errorf("get [answer_content] cache failed, err: %v", err)
 		}
-	default:
-		logger.Errorf("update question failed, err: %v", err)
-		res = &pb.UpdateAnswerRes{
-			Code: http.StatusInternalServerError,
-			Msg:  "internal err",
-			Ok:   false,
+		_, err = answerContentModel.WithContext(l.ctx).
+			Where(answerContentModel.AnswerID.Eq(in.AnswerId)).
+			Update(answerContentModel.Content, in.Content)
+		if err != nil {
+			if err == gorm.ErrRecordNotFound {
+				res = &pb.UpdateAnswerRes{
+					Code: http.StatusForbidden,
+					Msg:  "answer not found",
+					Ok:   false,
+				}
+				logger.Debugf("send message: %v", err)
+				return res, nil
+			} else {
+				logger.Errorf("update [answer_content] record failed, err: %v", err)
+				res = &pb.UpdateAnswerRes{
+					Code: http.StatusInternalServerError,
+					Msg:  "internal err",
+					Ok:   false,
+				}
+				logger.Debugf("send message: %v", err)
+				return res, nil
+			}
+		}
+
+		answerContent, err := answerContentModel.WithContext(l.ctx).
+			Where(answerContentModel.AnswerID.Eq(in.AnswerId)).
+			First()
+		if err != nil {
+			logger.Errorf("query [ansewr_content] record failed, err: %v", err)
+		} else {
+			answerContentBytes, err := proto.Marshal(&modelpb.AnswerContent{
+				AnswerId:   answerContent.AnswerID,
+				Content:    answerContent.Content,
+				Meta:       answerContent.Meta,
+				CreateTime: answerContent.CreateTime.String(),
+				UpdateTime: answerContent.UpdateTime.String(),
+			})
+			if err != nil {
+				logger.Errorf("marshal [answerContentProto] failed, err: %v", err)
+			} else {
+				err = l.svcCtx.Rdb.Set(l.ctx,
+					fmt.Sprintf("answer_content_%d", in.AnswerId),
+					answerContentBytes,
+					time.Second*86400).Err()
+				if err != nil {
+					logger.Errorf("set [answer_content] cache failed, err: %v", err)
+				}
+			}
 		}
 	}
 
+	res = &pb.UpdateAnswerRes{
+		Code: http.StatusOK,
+		Msg:  "update answer successfully",
+		Ok:   true,
+	}
 	logger.Debugf("send message: %v", res.String())
 	return res, nil
 }
--- app/service/question/rpc/crud/internal/logic/updatequestionlogic.go
+++ app/service/question/rpc/crud/internal/logic/updatequestionlogic.go
@@ -34,6 +34,11 @@ func (l *UpdateQuestionLogic) UpdateQuestion(in *pb.UpdateQuestionReq) (res *pb.
 	logger := log.GetSugaredLogger()
 	logger.Debugf("recv message: %v", in.String())
 
+	nowTime := time.Now()
+
+	questionSubjectModel := l.svcCtx.QuestionModel.QuestionSubject
+	questionContentModel := l.svcCtx.QuestionModel.QuestionContent
+
 	questionSubjectBytes, err := l.svcCtx.Rdb.Get(l.ctx,
 		fmt.Sprintf("question_subject_%d", in.QuestionId)).Bytes()
 	if err == nil {
@@ -45,65 +50,201 @@ func (l *UpdateQuestionLogic) UpdateQuestion(in *pb.UpdateQuestionReq) (res *pb.
 			questionSubjectProto.Tag = in.Tag
 			questionSubjectProto.Title = in.Title
 			questionSubjectProto.Topic = in.Topic
-		}
-	} else {
-		if err == redis.Nil {
+			questionSubjectProto.UpdateTime = nowTime.String()
+
+			questionSubjectBytes, err = proto.Marshal(questionSubjectProto)
+			if err != nil {
+				logger.Errorf("marshal [questionSubjectProto] failed, err: %v", err)
+				res = &pb.UpdateQuestionRes{
+					Code: http.StatusInternalServerError,
+					Msg:  "internal err",
+					Ok:   false,
+				}
+				logger.Debugf("send message: %v", err)
+				return res, nil
+			}
 			err = l.svcCtx.Rdb.Set(l.ctx,
 				fmt.Sprintf("question_subject_%d", in.QuestionId),
-				0,
+				questionSubjectBytes,
 				time.Second*86400).Err()
 			if err != nil {
 				logger.Errorf("set [question_subject] cache failed, err: %v", err)
+				res = &pb.UpdateQuestionRes{
+					Code: http.StatusInternalServerError,
+					Msg:  "internal err",
+					Ok:   false,
+				}
+				logger.Debugf("send message: %v", err)
+				return res, nil
 			}
-		} else {
+		}
+	} else {
+		if err != redis.Nil {
 			logger.Errorf("get [question_subject] cache failed, err: %v", err)
 		}
+		_, err = questionSubjectModel.WithContext(l.ctx).Select(
+			questionSubjectModel.ID,
+			questionSubjectModel.Title,
+			questionSubjectModel.Topic,
+			questionSubjectModel.Tag).
+			Where(questionSubjectModel.ID.Eq(in.QuestionId)).
+			Updates(model.QuestionSubject{
+				Title: in.Title,
+				Topic: in.Topic,
+				Tag:   in.Tag,
+			})
+		if err != nil {
+			if err == gorm.ErrRecordNotFound {
+				res = &pb.UpdateQuestionRes{
+					Code: http.StatusForbidden,
+					Msg:  "question not found",
+					Ok:   false,
+				}
+				logger.Debugf("send message: %v", err)
+				return res, nil
+			} else {
+				logger.Errorf("update [question_subject] record failed, err: %v", err)
+				res = &pb.UpdateQuestionRes{
+					Code: http.StatusInternalServerError,
+					Msg:  "internal err",
+					Ok:   false,
+				}
+				logger.Debugf("send message: %v", err)
+				return res, nil
+			}
+		}
+
+		questionSubject, err := questionSubjectModel.WithContext(l.ctx).
+			Where(questionSubjectModel.ID.Eq(in.QuestionId)).
+			First()
+		if err != nil {
+			logger.Errorf("query [question_subject] record failed, err: %v", err)
+		} else {
+			questionSubjectBytes, err := proto.Marshal(&modelpb.QuestionSubject{
+				Id:          questionSubject.ID,
+				UserId:      questionSubject.UserID,
+				IpLoc:       questionSubject.IPLoc,
+				Title:       questionSubject.Title,
+				Topic:       questionSubject.Topic,
+				Tag:         questionSubject.Tag,
+				SubCount:    questionSubject.SubCount,
+				AnswerCount: questionSubject.AnswerCount,
+				ViewCount:   questionSubject.ViewCount,
+				State:       questionSubject.State,
+				CreateTime:  questionSubject.CreateTime.String(),
+				UpdateTime:  questionSubject.UpdateTime.String(),
+			})
+			if err != nil {
+				logger.Errorf("marshal [questionSubjectProto] failed, err: %v", err)
+			} else {
+				err = l.svcCtx.Rdb.Set(l.ctx,
+					fmt.Sprintf("question_subject_%d", in.QuestionId),
+					questionSubjectBytes,
+					time.Second*86400).Err()
+				if err != nil {
+					logger.Errorf("set [question_subject] cache failed, err: %v", err)
+				}
+			}
+		}
 	}
 
-	questionSubjectModel := l.svcCtx.QuestionModel.QuestionSubject
-	questionContentModel := l.svcCtx.QuestionModel.QuestionContent
+	questionContentBytes, err := l.svcCtx.Rdb.Get(l.ctx,
+		fmt.Sprintf("question_content_%d", in.QuestionId)).Bytes()
+	if err == nil {
+		questionContentProto := &modelpb.QuestionContent{}
+		err = proto.Unmarshal(questionContentBytes, questionContentProto)
+		if err != nil {
+			logger.Errorf("unmarshal [questionSubjectProto] failed, err: %v", err)
+		} else {
+			questionContentProto.Content = in.Content
+			questionContentProto.UpdateTime = nowTime.String()
 
-	_, err = questionSubjectModel.WithContext(l.ctx).Select(
-		questionSubjectModel.ID,
-		questionSubjectModel.Title,
-		questionSubjectModel.Topic,
-		questionSubjectModel.Tag,
-	).
-		Where(questionSubjectModel.ID.Eq(in.QuestionId)).
-		Updates(model.QuestionSubject{
-			Title: in.Title,
-			Topic: in.Topic,
-			Tag:   in.Tag,
-		})
+			questionContentBytes, err = proto.Marshal(questionContentProto)
+			if err != nil {
+				logger.Errorf("marshal [questionContentProto] failed, err: %v", err)
+				res = &pb.UpdateQuestionRes{
+					Code: http.StatusInternalServerError,
+					Msg:  "internal er",
+					Ok:   false,
+				}
+				logger.Debugf("send message: %v", err)
+				return res, nil
+			}
+			err = l.svcCtx.Rdb.Set(l.ctx,
+				fmt.Sprintf("question_content_%d", in.QuestionId),
+				questionContentBytes,
+				time.Second*86400).Err()
+			if err != nil {
+				logger.Errorf("set [question_content] cache failed, err: %v", err)
+				res = &pb.UpdateQuestionRes{
+					Code: http.StatusInternalServerError,
+					Msg:  "internal er",
+					Ok:   false,
+				}
+				logger.Debugf("send message: %v", err)
+				return res, nil
+			}
+			res = &pb.UpdateQuestionRes{
+				Code: http.StatusOK,
+				Msg:  "update question successfully",
+				Ok:   true,
+			}
+			logger.Debugf("send message: %v", err)
+			return res, nil
+		}
+	} else {
+		if err != redis.Nil {
+			logger.Errorf("get [question_content] cache failed, err: %v", err)
+		}
+		_, err = questionContentModel.WithContext(l.ctx).Select(
+			questionContentModel.QuestionID,
+			questionContentModel.Content).
+			Where(questionContentModel.QuestionID.Eq(in.QuestionId)).
+			Update(questionContentModel.Content, in.Content)
+		if err != nil {
+			logger.Errorf("update [question_content] record failed, err: %v", err)
+			res = &pb.UpdateQuestionRes{
+				Code: http.StatusInternalServerError,
+				Msg:  "internal err",
+				Ok:   false,
+			}
+			logger.Debugf("send message: %v", err)
+			return res, nil
+		}
 
-	_, err = questionContentModel.WithContext(l.ctx).Select(
-		questionContentModel.QuestionID,
-		questionContentModel.Content,
-	).
-		Where(questionContentModel.QuestionID.Eq(in.QuestionId)).
-		Update(questionContentModel.Content, in.Content)
-	switch err {
-	case gorm.ErrRecordNotFound:
-		res = &pb.UpdateQuestionRes{
-			Code: http.StatusBadRequest,
-			Msg:  "question not found",
-			Ok:   false,
+		questionContent, err := questionContentModel.WithContext(l.ctx).
+			Where(questionContentModel.QuestionID.Eq(in.QuestionId)).
+			First()
+		if err != nil {
+			logger.Errorf("query [question_content] record failed, err: %v", err)
+		} else {
+			questionContentBytes, err := proto.Marshal(&modelpb.QuestionContent{
+				QuestionId: questionContent.QuestionID,
+				Content:    questionContent.Content,
+				Meta:       questionContent.Meta,
+				CreateTime: questionContent.CreateTime.String(),
+				UpdateTime: questionContent.UpdateTime.String(),
+			})
+			if err != nil {
+				logger.Errorf("marshal [questionContentProto] failed, err: %v", err)
+			} else {
+				err = l.svcCtx.Rdb.Set(l.ctx,
+					fmt.Sprintf("question_content_%d", in.QuestionId),
+					questionContentBytes,
+					time.Second*86400).Err()
+				if err != nil {
+					logger.Errorf("set [question_content] cache failed, err: %v", err)
+				}
+			}
 		}
-	case nil:
-		res = &pb.UpdateQuestionRes{
-			Code: http.StatusOK,
-			Msg:  "update question successfully",
-			Ok:   true,
-		}
-	default:
-		logger.Errorf("update question failed, err: %v", err)
-		res = &pb.UpdateQuestionRes{
-			Code: http.StatusInternalServerError,
-			Msg:  "internal err",
-			Ok:   false,
-		}
+
 	}
 
+	res = &pb.UpdateQuestionRes{
+		Code: http.StatusOK,
+		Msg:  "update question successfully",
+		Ok:   true,
+	}
 	logger.Debugf("send message: %v", res.String())
 	return res, nil
 }
